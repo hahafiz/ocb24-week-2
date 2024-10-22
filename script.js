a80a4445fcbf3d305749ffa23f9027feb8b5f169
hahafiz <hafizqamarul19@gmail.com>
2024-10-23 00:09:54 +0800

    week 2 assessment progress

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const envelope = document.getElementById("envelope");
-  setTimeout(() => {
-    envelope.style.opacity = 1;
-  }, 500);
-
-  const music = document.getElementById("bg-music");
-  music.volume = 0.1;
-});
